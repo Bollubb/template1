2b06aec92ed991b41e0050c3f9eb202546da87a3
Bollubb <sathorkenpachi@gmail.com>
2026-02-02 17:42:02 +0100

    Add PWA manifest head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from "next/head";
 import '@styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <Head>
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <meta name="theme-color" content="#0B1020" />
+      <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
+);
 }
