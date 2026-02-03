9f6f49e347b3b33c9fe1f4d2a0276c89f23f33ec
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 00:18:11 +0100

    Update BuyMeaCoffee.tsx

--- src/components/BuyMeaCoffee.tsx
+++ src/components/BuyMeaCoffee.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { AJAY_URL } from '@constants/index';
 import styles from '@styles/Bmc.module.css';
+import BuyMeaCoffeeSvg from '@svg/BuyMeaCoffeeSvg';
+
 
 /**
  * It renders a link to my Buy Me a Coffee page, with a coffee cup SVG inside it
