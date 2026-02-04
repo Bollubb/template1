6acc0a19e04d1d4a1759747d4fbe09f5747ddc81
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 20:56:57 +0100

    Add files via upload

---
+++ src/svg/BackArrowSvg.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function BackArrowSvg(): JSX.Element {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M15 18l-6-6 6-6" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
---
+++ src/svg/CatFourOFourSvg.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CatFourOFourSvg(): JSX.Element {
+  return (
+    <svg width="120" height="60" viewBox="0 0 120 60" aria-hidden="true">
+      <text x="0" y="40" fontSize="36">404</text>
+    </svg>
+  );
+}
