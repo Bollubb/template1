3747ae63f8a9fc123ffbe1ee8f8aea01b8fd6e97
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 21:06:39 +0100

    Update BackArrowSvg.tsx

--- src/svg/BackArrowSvg.tsx
+++ src/svg/BackArrowSvg.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-export default function BackArrowSvg(): JSX.Element {
+type Props = React.SVGProps<SVGSVGElement>;
+
+export default function BackArrowSvg(props: Props) {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" aria-hidden="true">
-      <path d="M15 18l-6-6 6-6" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      {...props}
+    >
+      <path
+        d="M15 18l-6-6 6-6"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 }
