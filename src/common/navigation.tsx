38583960043b9b034669820eba032a5f7827ade0
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 19:22:51 +0100

    new
    Define DOCS_URL and GITHUB_URL constants for navigation.

--- src/common/navigation.tsx
+++ src/common/navigation.tsx
@@ -1,14 +1,14 @@
 // src/common/navigation.tsx
 
-// Evita alias non configurati in build: usa path relativo affidabile
-import { DOCS_URL, GITHUB_URL } from '../constants';
-
 export type BottomNavigationItem = {
   title: string;
   href: string;
   external?: boolean;
   icon?: React.ReactNode;
 };
+
+const DOCS_URL = 'https://github.com/Bollubb/template1';
+const GITHUB_URL = 'https://github.com/Bollubb/template1';
 
 const bottomNavigation: BottomNavigationItem[] = [
   {
