70e9725e2fe1330de8dfa037069a19c7ddd8bf54
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 21:03:53 +0100

    Add files via upload

--- src/common/navigation.tsx
+++ src/common/navigation.tsx
@@ -1,26 +1,14 @@
-// src/common/navigation.tsx
+import { DOCS_URL, GITHUB_URL } from "@constants/index";
 
 export type BottomNavigationItem = {
   title: string;
   href: string;
   external?: boolean;
-  icon?: React.ReactNode;
 };
 
-const DOCS_URL = 'https://github.com/Bollubb/template1';
-const GITHUB_URL = 'https://github.com/Bollubb/template1';
-
 const bottomNavigation: BottomNavigationItem[] = [
-  {
-    title: 'Docs',
-    href: DOCS_URL,
-    external: true,
-  },
-  {
-    title: 'GitHub',
-    href: GITHUB_URL,
-    external: true,
-  },
+  { title: "Docs", href: DOCS_URL, external: true },
+  { title: "GitHub", href: GITHUB_URL, external: true },
 ];
 
 export default bottomNavigation;
