495665d53455e205617a39e92a95c536ddd0630c
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 20:52:43 +0100

    Add files via upload

---
+++ src/constants/nursediary/storageKeys.ts
@@ -0,0 +1,6 @@
+export const LS_PROFILE = "nursediary.profile";
+export const LS_PILLS = "nursediary.pills";
+export const LS_COLLECTION = "nursediary.collection";
+export const LS_RECENT_PULLS = "nursediary.recentPulls";
+export const LS_QUIZ_DAILY_DONE = "nursediary.quiz.dailyDone";
+export const LS_QUIZ_WEEKLY_DONE = "nursediary.quiz.weeklyDone";
