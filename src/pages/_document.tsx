faeee0daf236122c58a3688d5c0c25c1a5e342f3
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 19:14:03 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { Meta } from '@components/index';
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document(): JSX.Element {
+export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="it">
       <Head>
-        <Meta />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0b0f17" />
+
+        {/* iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="NurseDiary" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
       <body>
         <Main />
