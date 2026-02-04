9df4b83b337ca580c26831a0dd4cc6df63b78adb
Bollubb <sathorkenpachi@gmail.com>
2026-02-04 21:03:15 +0100

    Add files via upload

--- src/layouts/Section.tsx
+++ src/layouts/Section.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
+export type SectionProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
 /**
  * A React component that renders a section element.
- * @param  - `className` - the class name of the section
- * @returns A React component
  */
-function Section({ className, children }: cat.SectionProps): JSX.Element {
+export default function Section({ className, children }: SectionProps): JSX.Element {
   return <section className={className}>{children}</section>;
 }
-
-export default Section;
