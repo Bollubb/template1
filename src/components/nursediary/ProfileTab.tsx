ee398e86ef389b0c377ffd4eaffcca63e5f9951c
Bollubb <sathorkenpachi@gmail.com>
2026-02-16 17:18:58 +0100

    Add files via upload

--- src/components/nursediary/ProfileTab.tsx
+++ src/components/nursediary/ProfileTab.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
+import { getWeeklyChallenge, isWeeklyChallengeClaimed, claimWeeklyChallengeReward } from "@/features/social/weeklyChallenge";
 import { getCareer } from "@/features/career/career";
 
 import { useToast } from "./Toast";
