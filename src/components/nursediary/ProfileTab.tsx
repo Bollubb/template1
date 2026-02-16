9a031e5b1349ac1c4dc61182101b0a7288e671b3
Bollubb <sathorkenpachi@gmail.com>
2026-02-16 17:14:07 +0100

    Add files via upload

--- src/components/nursediary/ProfileTab.tsx
+++ src/components/nursediary/ProfileTab.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
+import { getCareer } from "@/features/career/career";
 
 import { useToast } from "./Toast";
 import MissionHub from "./MissionHub";
